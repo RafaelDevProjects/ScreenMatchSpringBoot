34ae47a744a524cecf10ca4cec6181b7dccead99
Rafael <rafaasigoli1@gmail.com>
2024-08-04 18:41:06 -0300

    screenMatchSpringBoot v1.02

--- src/main/java/br/com/rafael_tech/screenMatchSpringBoot/main/Main.java
+++ src/main/java/br/com/rafael_tech/screenMatchSpringBoot/main/Main.java
@@ -1,13 +1,19 @@
 package br.com.rafael_tech.screenMatchSpringBoot.main;
 
+import br.com.rafael_tech.screenMatchSpringBoot.model.Episode;
+import br.com.rafael_tech.screenMatchSpringBoot.model.EpisodeData;
 import br.com.rafael_tech.screenMatchSpringBoot.model.SeasonData;
 import br.com.rafael_tech.screenMatchSpringBoot.model.SerieData;
 import br.com.rafael_tech.screenMatchSpringBoot.services.ApiConsumption;
 import br.com.rafael_tech.screenMatchSpringBoot.services.ConvertData;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -46,6 +52,38 @@ public class Main {
 
         seasons.forEach(t -> t.episodes().forEach(e -> System.out.println("episode " + e.episodeNumber() + ": " + e.title())));
 
+        List<EpisodeData> episodesData = seasons.stream()
+                .flatMap(t -> t.episodes().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\n Top 5 episodes");
+        episodesData.stream()
+                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        List<Episode> episodes = seasons.stream()
+                .flatMap(t -> t.episodes().stream()
+                    .map(d -> new Episode(t.seasonNumber(), d))
+                ).collect(Collectors.toList());
+
+        episodes.forEach(System.out::println);
+
+        System.out.println("What year do you want to watch the episodes from?");
+        var year = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate searchDate = LocalDate.of(year, 1, 1);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodes.stream()
+                .filter(e -> e.getRealeaseDate() != null && e.getRealeaseDate().isAfter(searchDate))
+                .forEach(e -> System.out.println(
+                        "Season: " + e.getSeasonNumber() +
+                                " | Episode: " + e.getTitle() +
+                                " | Release year: " + e.getRealeaseDate().format(df)
+                ));
 
     }
 }
---
+++ src/main/java/br/com/rafael_tech/screenMatchSpringBoot/model/Episode.java
@@ -0,0 +1,85 @@
+package br.com.rafael_tech.screenMatchSpringBoot.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.SortedMap;
+
+public class Episode {
+    private String title;
+    private Integer seasonNumber;
+    private Integer episodeNumber;
+    private LocalDate realeaseDate;
+    private Double rating;
+    private String plot;
+
+    public Episode(Integer seasonNumber, EpisodeData episodeData) {
+        this.seasonNumber = seasonNumber;
+        this.title = episodeData.title();
+        this.episodeNumber = episodeData.episodeNumber();
+        try {
+            this.rating = Double.valueOf(episodeData.rating());
+            this.realeaseDate = LocalDate.parse(episodeData.realeaseDate());
+        } catch (NumberFormatException e) {
+            this.rating = 0.0;
+        } catch (DateTimeParseException e) {
+            this.realeaseDate = null;
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(Integer seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public Integer getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(Integer episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public LocalDate getRealeaseDate() {
+        return realeaseDate;
+    }
+
+    public void setRealeaseDate(LocalDate realeaseDate) {
+        this.realeaseDate = realeaseDate;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    @Override
+    public String toString() {
+        return "season: " + seasonNumber +
+                " | title: " + title +
+                " | episodeNumber=" + episodeNumber +
+                " | realeaseDate=" + realeaseDate +
+                " | rating=" + rating;
+    }
+}
